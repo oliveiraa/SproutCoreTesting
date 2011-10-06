de9c65f6612b27c2685bd9f0c1d2100ba937df91
Alvaro Oliveira <alvaro.oliveira@gmail.com>
2011-10-06 01:10:02 -0300

    First commit

---
+++ Sproutcore2Todo/Scripts/app.js
@@ -0,0 +1,27 @@
+﻿Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+    title: null,
+    isDone: false
+});
+
+Todos.todosController = SC.ArrayProxy.create({
+    content: [],
+
+    createTodo: function(title){
+        var todo = Todos.Todo.create({ title: title });
+        this.pushObject(todo);
+    }
+});
+
+Todos.CreateTodoView = SC.TextField.extend({
+    insertNewline: function () {
+        var value = this.get('value');
+
+        if (value) {
+            Todos.todosController.createTodo(value);
+            this.set('value', '');
+        }
+    }
+});
+
